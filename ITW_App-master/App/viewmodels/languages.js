77b0d658310a2790fcb872501895885b5c7ece12
Diogo <diogo.ramalho117@gmail.com>
2016-12-15 22:39:15 +0000

    added languages and changed icon

---
+++ ITW_App-master/App/viewmodels/languages.js
@@ -0,0 +1,56 @@
+define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko) {
+    //----------------------------------------------------------------------------------------------------------
+ var ctor = function(){
+        var self = this;
+        var searchLanguagesUri = 'http://192.168.160.39/api/Languages/Search/';
+        var languagesUri = 'http://192.168.160.39/api/Languages';
+        var languagesCountUri = 'http://192.168.160.39/api/Languages/Count';
+        self.searchText = ko.observable("");
+        self.languages = ko.observableArray();
+        self.languagesCount = ko.observable(null);
+        self.error = ko.observable();
+        self.searchTextGood = ko.computed(function () {
+            return (self.searchText().length < 3)
+        }, self);
+        //--- Funções internas
+        function ajaxHelper(uri, method, data) {
+            self.error(''); // Clear error message
+            return $.ajax({
+                type: method,
+                url: uri,
+                dataType: 'json',
+                contentType: 'application/json',
+                data: data ? JSON.stringify(data) : null,
+                error: function (jqXHR, textStatus, errorThrown) {
+                    console.log("AJAX Call[" + uri + "] Fail...");
+                    self.error(errorThrown);
+                }
+            })
+        };
+
+        //--- Funções visíveis do exterior
+        getAllLanguages = function () {
+            console.log('CALL: getAllLanguages...')
+            ajaxHelper(languagesCountUri, 'GET').done(function (data) {
+                self.languagesCount(data);
+            });
+            ajaxHelper(languagesUri, 'GET').done(function (data) {
+                self.languages(data);
+            });
+        };
+        clearLanguage = function () {
+            getAllLanguages();
+            self.searchText("");
+        };
+        searchLanguage = function () {
+            console.log('CALL: searchLanguages...')
+            ajaxHelper(searchLanguagesUri + self.searchText(), 'GET').done(function (data) {
+                self.languages(data);
+            });
+        };
+        //---- Chamada inicial
+        getAllLanguages();
+
+    };
+ return ctor;
+});
--- ITW_App-master/App/viewmodels/shell.js
+++ ITW_App-master/App/viewmodels/shell.js
@@ -7,9 +7,10 @@ define(['plugins/router', 'durandal/app'], function (router, app) {
                 { route: 'flickr', moduleId: 'viewmodels/flickr', nav: true, menu : '<i class="fa fa-flickr"></i>' },
                 { route: 'directors', moduleId: 'viewmodels/directors', nav: true, menu : '<i class="fa fa-film"></i>' },
                 { route: 'actors', moduleId: 'viewmodels/actors', nav: true, menu : '<i class="fa fa-users"></i>' },
-                { route: 'countries', moduleId: 'viewmodels/countries', nav: true, menu : '<i class="fa fa-fax"></i>' },
+                { route: 'countries', moduleId: 'viewmodels/countries', nav: true, menu : '<i class="fa fa-map"></i>' },
                 { route: 'movies', moduleId: 'viewmodels/movies', nav: true, menu : '<i class="fa fa-university"></i>' },
                 { route: 'genres', moduleId: 'viewmodels/genres', nav: true, menu : '<i class="fa fa-cc"></i>' },
+                { route: 'languages', moduleId: 'viewmodels/languages', nav: true, menu : '<i class="fa fa-sort-alpha-asc"></i>' },
                 { route: 'actorDetails(/:actorID)', moduleId: 'viewmodels/actorDetails', nav: false, menu : '<i class="fa fa-cc"></i>', hash: "#actorDetails"}
             ]).buildNavigationModel();
 
